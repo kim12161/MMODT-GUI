6013b1bad4a0eabf58f4bf7d1c974b603b721a15
Ren <auqona02@gmail.com>
2026-04-07 12:14:40 +0800

    yowyow

--- src/game/ScenePanel.java
+++ src/game/ScenePanel.java
@@ -295,7 +295,6 @@ public class ScenePanel extends JPanel {
         if (level == 1) {
             itemDiscoveryEvent();
         }
-
         for (int conversationNum = 1; conversationNum <= 3; conversationNum++) {
             if (!gameRunning) break;
 

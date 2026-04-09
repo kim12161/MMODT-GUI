063ecb136f879d07d65c6fbbfd887a7575876d72
Ren <auqona02@gmail.com>
2026-04-09 23:43:20 +0800

    yowyow

--- src/game/ScenePanel.java
+++ src/game/ScenePanel.java
@@ -246,9 +246,9 @@ public class ScenePanel extends JPanel {
         statusLabel = new JLabel("", SwingConstants.RIGHT);
         statusLabel.setFont(new Font(bFont, Font.PLAIN, 13));
         statusLabel.setForeground(new Color(220, 220, 220));
-        statusLabel.setBounds(660, 10, 380, 30);
-        statusLabel.setOpaque(false);
+        statusLabel.setBounds(260, 150, 380, 30);
 
+        statusLabel.setOpaque(false);
         add(statusLabel);
     }
 

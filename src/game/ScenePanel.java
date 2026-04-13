01e55e7ba5c5a753e71524f4bf87f67e4b86b4a1
kim12161 <kimmyswainalontaga@gmail.com>
2026-04-14 00:20:37 +0800

    added panels for manual buttons, etxx ka

--- src/game/ScenePanel.java
+++ src/game/ScenePanel.java
@@ -130,6 +130,7 @@ public class ScenePanel extends JPanel {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 Graphics2D g2 = (Graphics2D) g.create();
+
                 g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
 
                 g2.setColor(new Color(0, 0, 0, 60));

808190ff5dcf68b4fd61c71e58edd2d4634ae9ec
Ren <auqona02@gmail.com>
2026-05-05 12:47:47 +0800

    yowyow

--- src/Encounters/DeathPanel.java
+++ src/Encounters/DeathPanel.java
@@ -20,8 +20,8 @@ public class DeathPanel extends JPanel {
     private String bFont = "Munro";
 
     static {
-        loadFont("res/font/PixelArmy.ttf", 62f);
-        loadFont("res/font/Munro.ttf", 22f);
+        loadFont("/font/PixelArmy.ttf", 62f);
+        loadFont("/font/Munro.ttf", 22f);
     }
 
     private static Font loadFont(String path, float size) {

5ca1b48d3db9d67ebb5fed313b9b1b0c85252da6
Ren <auqona02@gmail.com>
2026-05-06 16:53:18 +0800

    yoowww

--- src/game/EndGamePanel.java
+++ src/game/EndGamePanel.java
@@ -118,7 +118,7 @@ public class EndGamePanel extends JPanel {
 
                     // Draw Profile Image
                     if (profileImg != null) {
-                        g2.drawImage(profileImg, 2, 2, getWidth() - 5, getHeight() - 5, this);
+                        g2.drawImage(profileImg, 2, 2, getWidth() - 2, getHeight() - 6, this);
                     }
                     g2.dispose();
                 }

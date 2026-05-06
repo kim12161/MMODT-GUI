1e878cad9475133a29c66f286617a22d633201b4
Ren <auqona02@gmail.com>
2026-05-06 14:17:51 +0800

    yoowww

--- src/Encounters/ZombieEncounterPanel.java
+++ src/Encounters/ZombieEncounterPanel.java
@@ -299,7 +299,7 @@ public class ZombieEncounterPanel extends JPanel {
         java.io.File f = new java.io.File("res/sprite/zombie.png");
         if (f.exists()) {
             ImageIcon raw = new ImageIcon(f.getAbsolutePath());
-            Image scaled = raw.getImage().getScaledInstance(380, 560, Image.SCALE_SMOOTH);
+            Image scaled = raw.getImage().getScaledInstance(380, 650, Image.SCALE_SMOOTH);
             zombieSprite.setIcon(new ImageIcon(scaled));
         }
         zombieSprite.setBounds(200, 80, 500, 600);

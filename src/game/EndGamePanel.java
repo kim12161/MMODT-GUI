e974642082f69e62a5132bff8b98b1edf6d166f4
Ren <auqona02@gmail.com>
2026-05-06 15:55:27 +0800

    yoowww

--- src/game/EndGamePanel.java
+++ src/game/EndGamePanel.java
@@ -298,7 +298,7 @@ public class EndGamePanel extends JPanel {
             int cropW = origW;
             int cropH = (int)(origH * 0.75); // ← increase to show more body, decrease for tighter face crop
             int cropX = 0;
-            int cropY = (int)(origH * 0.00); // ← start slightly from top to cut black space
+            int cropY = (int)(origH * 0.02); // ← start slightly from top to cut black space
 
             java.awt.image.BufferedImage cropped = new java.awt.image.BufferedImage(cropW, cropH, java.awt.image.BufferedImage.TYPE_INT_ARGB);
             Graphics2D cg = cropped.createGraphics();
